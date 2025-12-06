0311f955bce394a1f1bb8ab208af86ae0e4bc041
hadialhamza <hamzaglory@gmail.com>
2025-12-07 02:46:50 +0600

    create index.js and .env file. connect the server with mongodb.

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// MongoDB Connection String
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+
+    // Database & Collections
+    const database = client.db("bloodlineDB");
+    const usersCollection = database.collection("users");
+    const requestsCollection = database.collection("donationRequests");
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("BloodLine Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`BloodLine is running on port: ${port}`);
+});
